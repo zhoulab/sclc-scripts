60659230347459ad48b3070657c9032801458868
Victor Lin <vlin@ufl.edu>
2016-10-15 18:28:16 -0400

    Update README
    renamed run_all.sh -> run_all_hpc.sh

--- run_all_hpc.sh
+++ run_all_hpc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-while getopts "l:a:f" opt; do
+while getopts "g:l:a:f" opt; do
   case $opt in
+    g) gene_data="$OPTARG"
+    ;;
     l) gene_list="$OPTARG"
     ;;
     a) alpha="$OPTARG"
@@ -18,11 +20,11 @@ num_file="GenePairsNum_${gene_list}.txt"
 fisher_file="FisherGenePairs_${gene_list}.txt"
 fisher_file_filtered="FisherGenePairs_${gene_list}_${alpha}.txt"
 
-py_cmd="python -u gene_pairs.py -l $gene_list --calc_out $calc_file --num_out $num_file"
+py_cmd="python -u gene_pairs.py -g $gene_data -l $gene_list --calc_out $calc_file --num_out $num_file"
 result_dir="results/$gene_list"
 if [ "$filter" = true ]; then
     py_cmd="$py_cmd --filter_common"
-    result_dir="${result_dir}_remove_common"
+    result_dir="${result_dir}_removed_common"
 fi
 mkdir -p "$result_dir"
 echo "Results directory: $result_dir"
