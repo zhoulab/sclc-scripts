44010c78758947015c5fa81254e78e0d50bdc546
Victor Lin <vlin@ufl.edu>
2016-12-20 19:58:34 -0500

    Extend PyVCF's vcf.Reader
    see documentation in module for details

--- vcf/vcf_transform.py
+++ vcf/vcf_transform.py
@@ -6,65 +6,91 @@ import numpy as np
 from collections import OrderedDict
 from argparse import ArgumentParser
 
+from vcf import Reader
 
-def get_vcf_df(filename):
-    """Return pandas.DataFrame of VCF file.
 
-    * skip lines starting with '##'
-    * remove '#' from header line
+class VCFFile(Reader):
+    """Extension of PyVCF's Reader with pandas.DataFrame.
+
+
+    Additional attributes
+    ---------------------
+    see PyVCF documentation for Reader attributes.
+    df : pandas.DataFrame
+
+
+    Also handles no-call format strings within the DataFrame.
+
     """
-    with open(filename) as f:
-        # skip initial comment lines
-        skip_count = 0
-        line = f.readline()
-        while line[:2] == '##':
-            skip_count += 1
-            line = f.readline()
-        header = line.rstrip().split('\t')
-        if line[0] == '#':
-            header[0] = header[0][1:]
-        skip_count += 1
-    return pd.read_table(filename, header=None,
-                         names=header, skiprows=skip_count)
 
+    def __init__(self, filename, **kwargs):
+        """Must take filename for pandas.DataFrame loading."""
+        Reader.__init__(self, filename=filename, **kwargs)
+        self._init_df(filename)
 
-def transform_vcf_df(vcf_df):
-    """Return pandas.DataFrame of transformed VCF file.
+    def _init_df(self, filename):
+        """Initialize pandas.DataFrame of VCF file."""
+        self.df = pd.read_table(filename, header=None,
+                                names=(self._column_headers + self.samples),
+                                skiprows=(len(self._header_lines) + 1))
 
-    * replace './.:.:.:.:.:.' with NaN values for mutation
+    def get_maflite_df(self):
+        """Return melted pandas.DataFrame of VCF with MAFLITE columns.
 
-    Variables
-    ---------
-    transform_columns : OD
-        column definitions in the form
-        { transformed column : intiial column }
-    """
-    transform_columns = OrderedDict([('chr', 'CHROM'),
-                                     ('start', 'POS'),
-                                     ('end', 'POS'),
-                                     ('ref_allele', 'REF'),
-                                     ('alt_allele', 'ALT'),
-                                     ('patient', 'PATIENT')])
-    id_cols = map(str, vcf_df.columns[:9])
-    transform_df = pd.melt(vcf_df, id_vars=id_cols,
-                           var_name='PATIENT',
-                           value_name='MUTATION')
-    transform_df['MUTATION'].replace('./.:.:.:.:.:.', np.nan,
-                                     inplace=True)
-    transform_df = transform_df.loc[transform_df['MUTATION'].notnull(),
-                                    transform_columns.values()]
-    transform_df.columns = transform_columns.keys()
-    return transform_df
+        * replace no-call format strings with NaN values.
+
+        Variables
+        ---------
+        maflite_columns : OrderedDict
+            column definitions in the form
+            { transformed column : intiial column }
+        """
+        maflite_columns = OrderedDict([('chr', 'CHROM'),
+                                       ('start', 'POS'),
+                                       ('end', 'POS'),
+                                       ('ref_allele', 'REF'),
+                                       ('alt_allele', 'ALT'),
+                                       ('patient', 'SAMPLE')])
+        maflite_df = pd.melt(self.df, id_vars=self._column_headers,
+                             var_name='SAMPLE',
+                             value_name='VARIANT')
+        for no_call_string in self._get_no_call_strings():
+            maflite_df['VARIANT'].replace(no_call_string, np.nan,
+                                          inplace=True)
+        maflite_df = maflite_df.loc[maflite_df['VARIANT'].notnull(),
+                                    maflite_columns.values()]
+        maflite_df.columns = maflite_columns.keys()
+        return maflite_df
+
+    def _get_no_call_strings(self):
+        """Return no-call string for each format string in the format cache."""
+        # generate the cache
+        for x in self.next():
+            pass
+        for format_string in self._format_cache:
+            yield ':'.join([self._get_no_call_value(self.formats[x])
+                            for x in format_string.split(':')])
+
+    def _get_no_call_value(self, format_namedtuple):
+        """Return no-call string for a vcf.parser._Format namedtuple.
+
+        default: .
+
+        Specific formats
+        ----------------
+        GT:  ./.
+
+        """
+        if format_namedtuple.id == 'GT':
+            return './.'
+        return '.'
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Transform a VCF file.')
-    parser.add_argument('filename', help='input filename')
-    parser.add_argument('-o', '--output',
-                        help='output filename (default: input filename)')
+    parser = ArgumentParser(description='Generate MAFLITE file from VCF.')
+    parser.add_argument('filename', help='input VCF file')
+    parser.add_argument('output', help='output MAFLITE file')
     args = parser.parse_args()
-    if not args.output:
-        args.output = args.filename
-    vcf_df = get_vcf_df(args.filename)
-    transform_df = transform_vcf_df(vcf_df)
-    transform_df.to_csv(args.output, sep='\t', index=False)
+    vcf_file = VCFFile(args.filename)
+    maflite_df = vcf_file.get_maflite_df()
+    maflite_df.to_csv(args.output, sep='\t', index=False)
