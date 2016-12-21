a91f63c7585b3770aac04964280ce62c6bb9d9c0
Victor Lin <vlin@ufl.edu>
2016-12-21 18:43:40 -0500

    Add NaN conversion to VCFFile.df

--- vcf/vcf_transform.py
+++ vcf/vcf_transform.py
@@ -17,16 +17,29 @@ class VCFFile(Reader):
     ---------------------
     see PyVCF documentation for Reader attributes.
     df : pandas.DataFrame
-
+    is_nan_converted : specifies whether VCF empty values
+        have been converted to NaN
 
     Also handles no-call format strings within the DataFrame.
 
     """
 
-    def __init__(self, filename, **kwargs):
-        """Must take filename for pandas.DataFrame loading."""
+    def __init__(self, filename, convert_empty=True, **kwargs):
+        """Initialize VCFFile instance.
+
+        Parameters
+        ----------
+        filename : required for pandas.DataFrame loading
+        convert_empty : specify whether to convert
+            VCF file empty values to NaN
+        """
         Reader.__init__(self, filename=filename, **kwargs)
         self._init_df(filename)
+        if convert_empty:
+            self._convert_empty_to_nan()
+            self.is_nan_converted = True
+        else:
+            self.is_nan_converted = False
 
     def _init_df(self, filename):
         """Initialize pandas.DataFrame of VCF file."""
@@ -34,10 +47,14 @@ class VCFFile(Reader):
                                 names=(self._column_headers + self.samples),
                                 skiprows=(len(self._header_lines) + 1))
 
+    def _convert_empty_to_nan(self):
+        """Replace VCF empty values with NaN values."""
+        self.df.replace('.', np.nan, inplace=True)
+        for no_call_string in self._get_no_call_strings():
+            self.df.replace(no_call_string, np.nan, inplace=True)
+
     def get_maflite_df(self):
         """Return melted pandas.DataFrame of VCF with MAFLITE columns.
-
-        * replace no-call format strings with NaN values.
 
         Variables
         ---------
@@ -50,13 +67,14 @@ class VCFFile(Reader):
                                        ('end', 'POS'),
                                        ('ref_allele', 'REF'),
                                        ('alt_allele', 'ALT'),
-                                       ('patient', 'SAMPLE')])
+                                       ('sample', 'SAMPLE')])
         maflite_df = pd.melt(self.df, id_vars=self._column_headers,
                              var_name='SAMPLE',
                              value_name='VARIANT')
-        for no_call_string in self._get_no_call_strings():
-            maflite_df['VARIANT'].replace(no_call_string, np.nan,
-                                          inplace=True)
+        if not self.is_nan_converted:
+            for no_call_string in self._get_no_call_strings():
+                maflite_df['VARIANT'].replace(no_call_string, np.nan,
+                                              inplace=True)
         maflite_df = maflite_df.loc[maflite_df['VARIANT'].notnull(),
                                     maflite_columns.values()]
         maflite_df.columns = maflite_columns.keys()
