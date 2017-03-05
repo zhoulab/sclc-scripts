d9568a6fc24f8ea5a24c45eb6eb69211c9a46345
Victor Lin <vlin96@gmail.com>
2017-03-05 01:00:46 -0500

    Modify RB1 mutation subset script
    Extract all significant mutations from each set of samples (RB1-mutated
    and RB1-wild)

--- subset_rb1_sig_gene_mutations.py
+++ subset_rb1_sig_gene_mutations.py
@@ -10,12 +10,20 @@ genes = set(genes_df.gene.values)
 # subset mutation dataset for significant genes
 sig_gene_mutations = mutations[mutations['gene'].isin(genes)]
 
-# write RB1 mutations
+# get rb1 samples
 rb1_sig_gene_mutations = sig_gene_mutations[sig_gene_mutations['gene'] == 'RB1']
-rb1_sig_gene_mutations.to_csv('results/RB1_4dataset_nonsilent.tsv',
-                              sep='\t', index=False)
+rb1_samples = set(rb1_sig_gene_mutations['patient'].values)
+all_samples = set(sig_gene_mutations['patient'].values)
+print len(rb1_samples), 'out of', len(all_samples), 'samples have RB1 mutation'
 
-# write non-RB1 significant gene mutations
-not_rb1_sig_gene_mutations = sig_gene_mutations[sig_gene_mutations['gene'] != 'RB1']
-not_rb1_sig_gene_mutations.to_csv('results/SigGenes_005_notRB1_4dataset_nonsilent.tsv',
-                                  sep='\t', index=False)
+rb1_samples_sig_gene_mutations = sig_gene_mutations[sig_gene_mutations['patient'].isin(rb1_samples)]
+
+
+# write significant gene mutations from RB1 mutated samples
+rb1_samples_sig_gene_mutations.to_csv('results/SigGenes_005_B1_samples_4dataset_nonsilent.tsv',
+                                      sep='\t', index=False)
+
+# write significant gene mutations from non-RB1 mutated samples
+no_rb1_samples_sig_gene_mutations = sig_gene_mutations[~sig_gene_mutations['patient'].isin(rb1_samples)]
+no_rb1_samples_sig_gene_mutations.to_csv('results/SigGenes_005_not_RB1_samples_4dataset_nonsilent.tsv',
+                                         sep='\t', index=False)
